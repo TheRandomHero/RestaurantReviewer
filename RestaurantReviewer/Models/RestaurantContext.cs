bf178340c2904c6226395912ebec5330965c6570
Jonatán Uri <urijoni@gmail.com>
2019-09-10 14:41:00 +0200

    Restaurant Fields Done

---
+++ RestaurantReviewer/Models/RestaurantContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantReviewer.Models
+{
+    public class RestaurantContext : DbContext
+    {
+        public RestaurantContext(DbContextOptions<RestaurantContext> options) 
+            : base (options)
+        {
+        }
+
+        public DbSet<RestaurantItem> restaurantItems { get; set; }
+
+    }
+}
---
+++ RestaurantReviewer/Models/RestaurantItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantReviewer.Models
+{
+    public class RestaurantItem
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string ImgSource { get; set; }
+
+        public double FoodRating { get; set; }
+        public double ServiceRating { get; set; }
+        public double ValueRating { get; set; }
+        public double AtmposphereRating { get; set; }
+
+        public List<ReviewModel> Reviews { get; set; }
+
+        public RestaurantItem (string name, string imgSource)
+        {
+            this.Name = name;
+            this.ImgSource = imgSource;
+        }
+
+    }
+}
