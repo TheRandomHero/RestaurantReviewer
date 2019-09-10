871ce51779137d8c50910efda19447b84e5ab35b
Attila Molnar <scythianhawk@gmai.com>
2019-09-10 13:31:09 +0200

    Controller and model files added

---
+++ RestaurantReviewer/Models/ReviewContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantReviewer.Models
+{
+    public class ReviewContext
+    {
+    }
+}
