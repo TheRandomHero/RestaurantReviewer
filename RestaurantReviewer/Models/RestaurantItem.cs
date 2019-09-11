d46a553f9336e898b768fa8b6d220ed9a279b95c
Jonatán Uri <urijoni@gmail.com>
2019-09-11 14:02:38 +0200

    Added TotalRating to restaurants and rounding ratings

--- RestaurantReviewer/Models/RestaurantItem.cs
+++ RestaurantReviewer/Models/RestaurantItem.cs
@@ -11,12 +11,13 @@ namespace RestaurantReviewer.Models
         public string Name { get; set; }
         public string ImgSource { get; set; }
 
+        public double TotalRating { get; set; }
         public double FoodRating { get; set; }
         public double ServiceRating { get; set; }
         public double ValueRating { get; set; }
         public double AtmposphereRating { get; set; }
 
-        public List<ReviewItem> Reviews { get; set; }
+        public List<ReviewItem> Reviews { get; private set; }
 
         public RestaurantItem (string name, string imgSource)
         {
@@ -25,8 +26,8 @@ namespace RestaurantReviewer.Models
             Reviews = new List<ReviewItem>();
             if(Reviews.Count == 0)
             {
-                Reviews.Add(new ReviewItem(this.Id,"very good", "Nice, causy and very charming", 5, 4, 3, 5));
-                Reviews.Add(new ReviewItem(this.Id,"good", "Just like Mamma did it at home", 4, 4, 4, 3));
+                AddReview(new ReviewItem(this.Id,"very good", "Nice, causy and very charming", 5, 4, 3, 5));
+                AddReview(new ReviewItem(this.Id,"good", "Just like Mamma did it at home", 1, 4, 4, 3));
             }
         }
 
@@ -39,6 +40,10 @@ namespace RestaurantReviewer.Models
 
         public void CalculateRatingsFromReviews()
         {
+            FoodRating = 0;
+            ServiceRating = 0;
+            ValueRating = 0;
+            AtmposphereRating = 0;
             foreach (ReviewItem review in Reviews)
             {
                 FoodRating += review.FoodRating;
@@ -50,6 +55,17 @@ namespace RestaurantReviewer.Models
             ServiceRating /= Reviews.Count;
             ValueRating /= Reviews.Count;
             AtmposphereRating /= Reviews.Count;
+            TotalRating = (FoodRating + ServiceRating + ValueRating + AtmposphereRating) / 4;
+            RoundRatings();
+        }
+
+        private void RoundRatings()
+        {
+            FoodRating = Math.Round(FoodRating, 2);
+            ServiceRating = Math.Round(ServiceRating, 2);
+            ValueRating = Math.Round(ValueRating, 2);
+            AtmposphereRating = Math.Round(AtmposphereRating, 2);
+            TotalRating = Math.Round(TotalRating, 2);
         }
     }
 }
